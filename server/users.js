9a954f7664b4566a639da4e0ee367c070b60f2f1
Patrick Tunezerwane <tunezepatrick@gmail.com>
2019-12-15 18:18:18 +0200

    Write code for addingUser helper function

--- server/users.js
+++ server/users.js
@@ -1,7 +1,19 @@
 const users = [];
 
-const addingUser = () => {
+const addingUser = ({ id, username, chatRoom }) => {
+  username = username.trim().toLowerCase();
+  chatRoom = chatRoom.trim().toLowerCase();
 
+  const isUserExist = users.find((user) => user.chatRoom === chatRoom && user.username === username);
+
+  if(isUserExist) {
+    return { error: 'Username is in use, pls try an other one' };
+  }
+
+  const user = { id, username, chatRoom };
+  users.push(user);
+  
+  return  { user }
 }
 
 const removingUser = () => {
@@ -13,5 +25,5 @@ const gettingUser = () => {
 }
 
 const gettingUsersFromRoom = () => {
-  
+
 }
