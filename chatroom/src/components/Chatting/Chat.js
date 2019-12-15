edb669748cef499252bd3bc91014e42e5a17b2da
Patrick Tunezerwane <tunezepatrick@gmail.com>
2019-12-15 16:56:55 +0200

    Initiate connections and disconnections of io (the socket.io instance)

--- chatroom/src/components/Chatting/Chat.js
+++ chatroom/src/components/Chatting/Chat.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 
-const Chat = () => {
+let theSocket;
+
+const Chat = ({ location }) => {
+  const [username, setUserName] = useState('');
+  const [chatRoom, setchatRoom] = useState('');
+  const url = 'localhost:5500';
+
+  useEffect(() => {
+    const { username, chatRoom } = queryString.parse(location.search);
+
+    theSocket = io(url);
+
+    setUserName(username);
+    setchatRoom(chatRoom);
+
+    theSocket.emit('join', { username, chatRoom });
+  }, [url, location.search]);
+
   return (
     <h1>This is the chat component</h1>
   )
