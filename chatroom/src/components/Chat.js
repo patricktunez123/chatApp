840ed8622df8043b2e6df54d1e97d65e0de7b151
Patrick Tunezerwane <tunezepatrick@gmail.com>
2019-12-15 12:35:36 +0200

    Set up and test the components

---
+++ chatroom/src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <h1>This is the chat component</h1>
+  )
+}
+
+export default Chat;
---
+++ chatroom/src/components/Join.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Join = () => {
+  return (
+    <h1>This is the Join component</h1>
+  )
+}
+
+export default Join;
