a08e0f90991f30556ae6436e017d958c252bdc38
Patrick Tunezerwane <tunezepatrick@gmail.com>
2019-12-15 14:20:51 +0200

    Integrate socket.io
    [Finishes #170301156]

--- server/index.js
+++ server/index.js
@@ -9,5 +9,13 @@ const app = express();
 const srv = http.createServer(app);
 const io = socketio(srv);
 
+io.on('connection', (socket) => {
+  console.log('new join');
+
+  socket.on('disconnect', () => {
+    console.log('left the conversation');
+  })
+})
+
 app.use(router);
 srv.listen(port, () => console.log(`Server in listening on port: ${port}`));
