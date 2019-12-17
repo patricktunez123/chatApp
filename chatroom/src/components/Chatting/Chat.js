08dd49849e719f2a94148ff90bfd44619ab4dac9
Patrick Tunezerwane <tunezepatrick@gmail.com>
2019-12-17 19:55:15 +0200

    Set users

--- chatroom/src/components/Chatting/Chat.js
+++ chatroom/src/components/Chatting/Chat.js
@@ -12,6 +12,7 @@ let theSocket;
 const Chat = ({ location }) => {
   const [username, setUserName] = useState('');
   const [chatRoom, setchatRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const url = 'localhost:5500';
@@ -38,6 +39,16 @@ const Chat = ({ location }) => {
     theSocket.on('message', (message) => {
       setMessages([...messages, message]);
     })
+
+    theSocket.on('chatRoomData', ({ users }) => {
+      setUsers(users);
+    })
+
+    return () => {
+      theSocket.emit('disconnect');
+
+      theSocket.off();
+    }
   }, [messages]);
 
   const sendMsg = (event) => {
