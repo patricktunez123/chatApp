0211db23ab7f58152cd3b300bf3e675e1945f568
Patrick Tunezerwane <tunezepatrick@gmail.com>
2019-12-18 18:41:37 +0200

    Add useState for password

--- chatroom/src/components/Join/Join.js
+++ chatroom/src/components/Join/Join.js
@@ -5,13 +5,15 @@ import './Join.css';
 const Join = () => {
   const [username, setUserName] = useState('');
   const [chatRoom, setchatRoom] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className="outerContainer">
       <div className="innerContainer">
         <h5 className="headingTitle">Connect with your friends</h5>
         <div><input placeholder="Username" className="joiningInput" type="text" onChange={(event) => setUserName(event.target.value)} /></div>
         <div><input placeholder="ChatRoom" className="joiningInput mt-20" type="text" onChange={(event) => setchatRoom(event.target.value)} /></div>
-        <Link onClick={event => (!username || !chatRoom) ? event.preventDefault() : null} to={`/Chat?username=${username}&chatRoom=${chatRoom}`}>
+        <div><input placeholder="Password" className="joiningInput mt-20" type="text" onChange={(event) => setPassword(event.target.value)} /></div>
+        <Link onClick={event => (!username || !chatRoom || !password) ? event.preventDefault() : null} to={`/Chat?username=${username}&chatRoom=${chatRoom}&password=${password}`}>
           <button className="btn mt-20" type="submit">Login</button>
         </Link>
       </div>
